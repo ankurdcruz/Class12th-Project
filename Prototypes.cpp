db748e26caf54f3f5c5bbf7321d1f98c6169c5b3
ankurdcruz <42521279+ankurdcruz@users.noreply.github.com>
2019-06-27 21:31:10 +0530

    Add files via upload
    Prototypes and their functions

---
+++ Prototypes.cpp
@@ -0,0 +1,15 @@
+void border();                   //To create border
+void pageend();                  //For page transitions
+void question();                 //For question mark design
+int noofwords();                 //For no of words 
+int pass();                      //For password
+void book();                     //For book design
+void smartpattern();             //For smart search design
+void create();                   //To create file
+void move(char c,int  a);        //For move function
+void add();                      //To add words
+void smart();                    //For smart search function
+void random();                   //To get random words
+void all();                      //To view all records
+void pattern();                  //For pattern  
+
